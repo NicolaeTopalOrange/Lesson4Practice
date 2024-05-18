2a1594ac181ff04df164fc222ebc9d8b77419f1f
Nicolae Topal <nicolae.topal@orange.com>
2024-05-18 13:47:12 +0300

    steps

--- src/test/java/tests/TestReqres.java
+++ src/test/java/tests/TestReqres.java
@@ -1,20 +1,41 @@
 package tests;
 
+import io.qameta.allure.Allure;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.openqa.selenium.devtools.v85.fetch.model.AuthChallengeResponse;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static utils.Steps.GET;
+import static utils.Steps.isStatusCodeValid;
 
 public class TestReqres {
+
+    @BeforeSuite
+    public void setUp(){
+        baseURI = "https://reqres.in/api";
+    }
 
     @Test
     public void testListUsers() {
 
-        given().
-                when().
-                get("https://reqres.in/api/users?page=2").
-                then().
-                assertThat().
-                statusCode(200);
+        String url = "/users?page=2";
+
+        Response response = GET(url);
+
+        isStatusCodeValid(response, 200);
+
+    }
+
+    public void testSingleUser(){
+        String url = "/users/2";
+
+        Response response = GET(url);
+
+        isStatusCodeValid(response, 200);
     }
 
 }
