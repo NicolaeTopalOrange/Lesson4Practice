21f503a3f6cb01e865c2426fff81d809a87b6964
Nicolae Topal <nicolae.topal@orange.com>
2024-05-18 11:24:56 +0300

    initial commit

---
+++ src/test/java/tests/TestReqres.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestReqres {
+
+    @Test
+    public void testTestNg() {
+        System.out.println("Hello World");
+    }
+
+}
