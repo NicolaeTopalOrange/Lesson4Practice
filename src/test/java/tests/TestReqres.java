c996e3935645bc47bd518fb33fe9e8ccc5dccd12
Nicolae Topal <nicolae.topal@orange.com>
2024-05-18 12:40:57 +0300

    add first test

--- src/test/java/tests/TestReqres.java
+++ src/test/java/tests/TestReqres.java
@@ -2,11 +2,19 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestReqres {
 
     @Test
-    public void testTestNg() {
-        System.out.println("Hello World");
+    public void testListUsers() {
+
+        given().
+                when().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                assertThat().
+                statusCode(200);
     }
 
 }
